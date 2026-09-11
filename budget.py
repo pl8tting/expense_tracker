23cec1f06e07948b9fd43710d56741a35ed3f67f
pl8tting <218480901+pl8tting@users.noreply.github.com>
2026-09-11 12:47:32 +0100

    Expense tracker i just made

---
+++ budget.py
@@ -0,0 +1,63 @@
+import json
+try:
+    with open('save.json','r') as f:
+        expenses = json.load(f)
+except json.decoder.JSONDecodeError:
+    expenses = []
+def add_expense():
+    while True:
+        name = input("Name of the expense? ")
+        while True:
+            try:
+                price = float(input("And how much does it cost? "))
+                break
+            except ValueError:
+                print("Please make sure what you entered only contains numbers")
+        category = input("And what category does it fall under? ")
+
+        new_dictionary = {
+            'Name':name,
+            'Price':float(price),
+            'Category':category
+        }
+        
+        expenses.append(new_dictionary)
+        answer = input("Would you like to add anything else? Yes[Y] / No[N] ")
+        if answer.lower() == 'n':
+            break
+    
+def view_expenses():
+    print("===Current Expenses===")
+    for i in expenses:
+        for key,value in i.items():
+            print(f'{key}: {value}')
+
+def view_expenses_category():
+    category = input("What category would you like to see? ")
+    total_expense = 0
+    for i in expenses:
+        if i["Category"] == category:
+            total_expense += i["Price"]
+    print(f"Total expenses in the {category} category is {total_expense}")
+
+def save():
+    with open('save.json','w') as f:
+        json.dump(expenses,f)
+    print("Saved!")
+
+actions = {
+    '1': add_expense,
+    '2': view_expenses,
+    '3': view_expenses_category,
+    '4': save
+    
+}
+
+while True:
+    choice = input("What would you like to do? \n [1] Add expense \n [2] View expenses \n [3] View expenses by category \n [4] Save \n [5] Quit \n")
+    if choice == '5':
+        break
+    elif choice in actions:
+        actions[choice]()
+    else:
+        print("Please type either 1,2,3 or 4")
